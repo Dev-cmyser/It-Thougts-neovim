5f0cd8ed5241698c4b8bbfc525830693f43c60ed
Dev-cmyser <kirrallt@mail.ru>
2023-10-23 16:09:41 +0300

    first commit

---
+++ lua/core/mappings.lua
@@ -0,0 +1,34 @@
+vim.g.mapleader = " "
+
+-- NeoTree
+vim.keymap.set('n', '<leader>E', ':Neotree float reveal<CR>')
+vim.keymap.set('n', '<leader>e', ':Neotree left reveal<CR>')
+vim.keymap.set('n', '<leader>o', ':Neotree float git_status<CR>')
+
+-- Navigation
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+vim.keymap.set('n', '<leader>/', ':CommentToggle<CR>')
+
+-- Splits
+vim.keymap.set('n', '|', ':vsplit<CR>')
+vim.keymap.set('n', '\\', ':split<CR>')
+
+-- Other
+vim.keymap.set('n', '<leader>w', ':w<CR>')
+vim.keymap.set('n', '<leader>x', ':BufferLinePickClose<CR>')
+vim.keymap.set('n', '<leader>X', ':BufferLineCloseRight<CR>')
+vim.keymap.set('n', '<leader>s', ':BufferLineSortByTabs<CR>')
+vim.keymap.set('i', 'jj', '<Esc>')
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+
+-- Tabs
+vim.keymap.set('n', '<Tab>', ':BufferLineCycleNext<CR>')
+vim.keymap.set('n', '<s-Tab>', ':BufferLineCyclePrev<CR>')
+
+-- Terminal
+vim.keymap.set('n', '<F7>', ':ToggleTerm direction=float<CR>')
+vim.keymap.set('n', '<leader>th', ':ToggleTerm direction=horizontal<CR>')
+vim.keymap.set('n', '<leader>tv', ':ToggleTerm direction=vertical size=40<CR>')
---
+++ lua/plugins/comments.lua
@@ -0,0 +1,4 @@
+require('nvim_comment').setup({
+    line_mapping = "<leader>c",
+    operator_mapping = "<leader>c"
+})
