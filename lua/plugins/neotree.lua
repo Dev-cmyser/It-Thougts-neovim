a8756c3e34ca85940671e8222391fe645c35ad5c
Dev-cmyser <kirrallt@mail.ru>
2023-10-25 13:30:00 +0300

    neotree open by w and o

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -20,30 +20,19 @@ vim.fn.sign_define("DiagnosticSignInfo",
 vim.fn.sign_define("DiagnosticSignHint",
                    {text = "", texthl = "DiagnosticSignHint"})
 
-require("neo-tree").setup({update_focused_file = {
-    enable = true,
-    update_cwd = true, -- Активировать обновление CWD
-  },buffers = {
-          follow_current_file = {
-            enabled = true, -- This will find and focus the file in the active buffer every time
-            --              -- the current file is changed while the tree is open.
-            leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
-          },
-          group_empty_dirs = true, -- when true, empty folders will be grouped together
-          show_unloaded = true,
-          window = {
-            mappings = {
-              ["bd"] = "buffer_delete",
-              ["<bs>"] = "navigate_up",
-              ["."] = "set_root",
-              ["o"] = { "show_help", nowait=false, config = { title = "Order by", prefix_key = "o" }},
-              ["oc"] = { "order_by_created", nowait = false },
-              ["od"] = { "order_by_diagnostics", nowait = false },
-              ["om"] = { "order_by_modified", nowait = false },
-              ["on"] = { "order_by_name", nowait = false },
-              ["os"] = { "order_by_size", nowait = false },
-              ["ot"] = { "order_by_type", nowait = false },
-            }
-          },
-        },
+require("neo-tree").setup({
+    update_focused_file = {
+        enable = true,
+        update_cwd = true, -- Активировать обновление CWD
+    },
+    window = {
+        mappings = {
+            ["o"] = "open",
+            ["w"] = "open",-- открыть/закрыть файл или директорию
+
+
+        }
+    },
 })
+
+
