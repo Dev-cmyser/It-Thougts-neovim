4c2b30e5df9732979e5ea8d672bf6094a7d50013
Dev-cmyser <kirrallt@mail.ru>
2023-10-27 23:57:18 +0300

    vue puthon go runst

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,6 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "typescript", "lua", "go", "tsx", "javascript" },
+  ensure_installed = { "typescript", "lua", "go", "tsx", "javascript", "vue", "css", "html",  "rust", "python" },
+  ignore_install = {},
 
   sync_install = false,
   auto_install = true,
