579dc369b402ead4710fc6fe5c338b5e33091a16
Dev-cmyser <kirrallt@mail.ru>
2023-11-01 16:41:36 +0300

    opacity background

--- lua/core/colors.lua
+++ lua/core/colors.lua
@@ -1,15 +1,15 @@
 vim.opt.termguicolors = true
 
 function SetColor(color)
-    color = color or "onedark"
-    vim.cmd.colorscheme(color)
+	color = color or "onedark"
+	vim.cmd.colorscheme(color)
 
-    -- vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
-    -- vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
-    -- vim.api.nvim_set_hl(0, "ColorColumn", {bg = "none"})
-    -- vim.api.nvim_set_hl(0, "LineNr", {bg = "none"})
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	vim.api.nvim_set_hl(0, "ColorColumn", { bg = "none" })
+	vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
 end
 
-SetColor('kanagawa-wave')
+SetColor("kanagawa-wave")
 -- SetColor('onenord')
 -- SetColor('onedark')
