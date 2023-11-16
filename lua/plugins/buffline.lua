14f6bdd658362fb3ad44b12be6fa424da131a1d1
Dev-cmyser <kirrallt@mail.ru>
2023-11-16 18:17:50 +0300

    style buffer

--- lua/plugins/buffline.lua
+++ lua/plugins/buffline.lua
@@ -1,19 +1,22 @@
 require("bufferline").setup({
 	options = {
 		buffer_close_icon = "",
+		-- buffer_close_icon = "󰅙",
 		mode = "buffers",
 		offsets = {
 			{
 				filetype = "neo-tree",
 				text = "File Explorer",
 				separator = true,
-				padding = 1,
+				padding = 5,
 			},
 		},
 		diagnostics = "nvim_lsp",
 		indicator = {
-			icon = "", -- this should be omitted if indicator style is not 'icon'
+			-- icon = "", -- this should be omitted if indicator style is not 'icon'
+			-- icon = "", -- this should be omitted if indicator style is not 'icon'
 			style = "icon",
+			-- padding = { left = 0, right = 0, top = 0, bottom = 0 },
 		},
 	},
 })
