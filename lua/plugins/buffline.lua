c22ec1a8b7671f2047dca0a7a4d3c2abdfc35a4f
Dev-cmyser <kirrallt@mail.ru>
2023-11-16 16:37:23 +0300

    лишнее

--- lua/plugins/buffline.lua
+++ lua/plugins/buffline.lua
@@ -15,13 +15,5 @@ require("bufferline").setup({
 			icon = "", -- this should be omitted if indicator style is not 'icon'
 			style = "icon",
 		},
-		close_command = function(bufnum)
-			-- Сохраните позицию курсора
-			local cursor_pos = vim.api.nvim_win_get_cursor(0)
-			-- Закройте буфер
-			vim.cmd("bdelete! " .. bufnum)
-			-- Восстановите позицию курсора в новом буфере
-			vim.api.nvim_win_set_cursor(0, cursor_pos)
-		end, -- separator_style = "slope"
 	},
 })
