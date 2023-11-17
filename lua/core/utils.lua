2dcbbca23e0202f25290910868823e87c36651cc
Dev-cmyser <kirrallt@mail.ru>
2023-11-17 16:05:14 +0300

    vue lsp

--- lua/core/utils.lua
+++ lua/core/utils.lua
@@ -11,16 +11,16 @@
 --
 -- 	for i, buf in ipairs(buf_list) do
 -- 		if buf.bufnr == current_buf then
--- 			-- Если текущий буфер найден, закрываем его
+-- 			-- если текущий буфер найден, закрываем его
 -- 			vim.cmd("bdelete " .. current_buf)
 --
 -- 			if vim.b.cursor_pos then
 -- 				vim.api.nvim_win_set_cursor(0, vim.b.cursor_pos)
 -- 			end
--- 			-- Переходим к следующему буферу справа, если он существует
+-- 			-- переходим к следующему буферу справа, если он существует
 -- 			if i < #buf_list then
 -- 				vim.cmd("buffer " .. buf_list[i + 1].bufnr)
--- 			else -- Иначе переходим к предыдущему буферу слева
+-- 			else -- иначе переходим к предыдущему буферу слева
 -- 				vim.cmd("buffer " .. buf_list[i - 1].bufnr)
 -- 			end
 -- 			break
@@ -41,35 +41,30 @@ _G.close_and_go_right = function()
 		return
 	end
 
-	-- Сохраняем текущую позицию курсора для текущего буфера
-	-- _G.cursor_positions[current_buf] = vim.api.nvim_win_get_cursor(0)
-
-	-- Находим следующий буфер
+	-- -- Если только один буфер, закрываем и создаем новый
+	-- if #buf_list == 1 then
+	-- 	vim.cmd("enew") -- открывает новый пустой буфер
+	-- 	vim.cmd("bdelete " .. current_buf)
+	-- 	return
+	-- end
+	-- Находим следующий или предыдущий буфер
 	local next_bufnr
 	for i, buf in ipairs(buf_list) do
 		if buf.bufnr == current_buf then
-			next_bufnr = buf_list[i % #buf_list + 1].bufnr
+			if i == #buf_list then
+				-- Если текущий буфер последний, идем к предыдущему
+				next_bufnr = buf_list[i - 1].bufnr
+			else
+				-- Иначе идем к следующему
+				next_bufnr = buf_list[i % #buf_list + 1].bufnr
+			end
 			break
 		end
 	end
 
-	-- Переходим к следующему буферу и закрываем текущий
+	-- Переходим к выбранному буферу и закрываем текущий
 	vim.cmd("buffer " .. next_bufnr)
 	vim.cmd("bdelete " .. current_buf)
-
-	-- Восстанавливаем позицию курсора для нового буфера, если она была сохранена
-	-- local new_cursor_pos = _G.cursor_positions[next_bufnr]
-	-- if new_cursor_pos then
-	-- 	local win_id = vim.fn.bufwinid(next_bufnr)
-	-- 	if win_id ~= -1 then
-	-- 		-- Убедимся, что позиция курсора находится в пределах нового буфера
-	-- 		local line_count = vim.api.nvim_buf_line_count(next_bufnr)
-	-- 		new_cursor_pos[1] = math.min(new_cursor_pos[1], line_count)
-	-- 		local col_count = #vim.api.nvim_buf_get_lines(next_bufnr, new_cursor_pos[1] - 1, new_cursor_pos[1], true)[1]
-	-- 		new_cursor_pos[2] = math.min(new_cursor_pos[2], col_count)
-	-- 		vim.api.nvim_win_set_cursor(win_id, new_cursor_pos)
-	-- 	end
-	-- end
 end
 
 vim.g.neotree_open = false
