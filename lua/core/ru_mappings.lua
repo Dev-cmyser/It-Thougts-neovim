0c7c17d80249434f53bc8bf5894584f5f3190c66
Dev-cmyser <kirrallt@mail.ru>
2023-11-24 11:47:42 +0300

    fix a little

--- lua/core/ru_mappings.lua
+++ lua/core/ru_mappings.lua
@@ -1,89 +1,90 @@
 -- Русский язык
 
+local map = vim.keymap.set
 -- Сохранение файла
-vim.keymap.set("n", "<leader>ц", ":w<CR>")
+map("n", "<leader>ц", ":w<CR>")
 
 -- КОментирование ( любое количество строк )
-vim.keymap.set("n", "<leader>.", ":CommentToggle<CR>")
-vim.keymap.set("v", "<leader>.", ":CommentToggle<CR>")
+map("n", "<leader>.", ":CommentToggle<CR>")
+map("v", "<leader>.", ":CommentToggle<CR>")
 
 -- В Нормальный режим
-vim.keymap.set("i", "оо", "<Esc>")
+map("i", "оо", "<Esc>")
 
 -- Перемещение влево (аналог клавиши 'h')
-vim.keymap.set("n", "р", "h")
-vim.keymap.set("v", "р", "h")
+map("n", "р", "h")
+map("v", "р", "h")
 
 -- Перемещение вниз (аналог клавиши 'j')
-vim.keymap.set("n", "о", "j")
-vim.keymap.set("v", "о", "j")
+map("n", "о", "j")
+map("v", "о", "j")
 
 -- Перемещение вверх (аналог клавиши 'k')
-vim.keymap.set("n", "л", "k")
-vim.keymap.set("v", "л", "k")
+map("n", "л", "k")
+map("v", "л", "k")
 
 -- Перемещение вправо (аналог клавиши 'l')
-vim.keymap.set("n", "д", "l")
-vim.keymap.set("v", "д", "l")
+map("n", "д", "l")
+map("v", "д", "l")
 
 -- Удаление
-vim.keymap.set("n", "в", "d")
-vim.keymap.set("v", "в", "d")
-vim.keymap.set("n", "вв", "dd")
-vim.keymap.set("n", "ч", "x")
+map("n", "в", "d")
+map("v", "в", "d")
+map("n", "вв", "dd")
+map("n", "ч", "x")
 
 -- Go to Insert mode
-vim.keymap.set("n", "ф", "a")
-vim.keymap.set("n", "щ", "o")
-vim.keymap.set("n", "Щ", "O")
-vim.keymap.set("n", "ш", "i")
-vim.keymap.set("v", "с", "c")
+map("n", "ф", "a")
+map("n", "щ", "o")
+map("n", "Щ", "O")
+map("n", "ш", "i")
+map("v", "с", "c")
 
 -- Вставка
-vim.keymap.set("n", "з", "p")
-vim.keymap.set("v", "з", "p")
+map("n", "з", "p")
+map("v", "з", "p")
 
 -- Отмена действия
-vim.keymap.set("n", "г", "u")
+map("n", "г", "u")
 
 -- Копирование
-vim.keymap.set("n", "нн", "yy")
-vim.keymap.set("n", "н", "y")
-vim.keymap.set("v", "н", "yy")
+map("n", "нн", "yy")
+map("n", "н", "y")
+map("v", "н", "yy")
 
 -- Замена replace
-vim.keymap.set("n", "к", "r")
+map("n", "к", "r")
 
 -- Пермещение между вкладками ( буфферами )
-vim.keymap.set("n", "Р", ":BufferLineCyclePrev<CR>:Neotree show reveal<CR>")
-vim.keymap.set("n", "Д", ":BufferLineCycleNext<CR>:Neotree show reveal<CR>")
+map("n", "Р", ":BufferLineCyclePrev<CR>:Neotree show reveal<CR>")
+map("n", "Д", ":BufferLineCycleNext<CR>:Neotree show reveal<CR>")
 
 -- Закрытие всех буферов
-vim.keymap.set("n", "<leader>ч", ":BufferLineSortByTabs<CR>:BufferLineCloseRight<CR>")
+map("n", "<leader>ч", ":BufferLineSortByTabs<CR>:BufferLineCloseRight<CR>")
 -- одного
-vim.keymap.set("n", "<leader>Ч", ":BufferLinePickClose<CR>")
+map("n", "<leader>Ч", ":BufferLinePickClose<CR>")
 
--- ВыделениЧ
-vim.keymap.set("n", "м", "v")
-vim.keymap.set("v", "м", "v")
-vim.keymap.set("n", "М", "V")
-vim.keymap.set("v", "М", "V")
+-- Выделение
+map("n", "м", "v")
+map("v", "м", "v")
+map("n", "М", "V")
+map("v", "М", "V")
 
 -- Дерево
-vim.keymap.set("n", "<leader>у", ":Neotree toggle<CR>")
-vim.keymap.set("n", "<leader>щ", ":Neotree float git_status<CR>")
+map("n", "<leader>у", ":Neotree toggle<CR>")
+map("n", "<leader>щ", ":Neotree float git_status<CR>")
 
 -- Команды
-vim.keymap.set("n", "Ж", ":")
+map("n", "Ж", ":")
 
 -- Удаление буфера
 vim.api.nvim_set_keymap("n", "<leader>с", ":lua _G.close_and_go_right()<CR>", { noremap = true, silent = true })
 
 -- В начало и конец файла
-vim.keymap.set("n", "пп", "gg")
-vim.keymap.set("v", "пп", "gg")
-vim.keymap.set("n", "П", "G")
-vim.keymap.set("v", "П", "G")
+map("n", "пп", "gg")
+map("v", "пп", "gg")
+map("n", "П", "G")
+map("v", "П", "G")
 
 -- Показать файл в дереве
-vim.keymap.set("n", "<leader>ы", ":Neotree show reveal <CR>")
+map("n", "<leader>ы", ":Neotree show reveal <CR>")
