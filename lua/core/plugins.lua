4aad31f8c7dab2f4e5f3356b595d54dcf761a247
Dev-cmyser <kirrallt@mail.ru>
2023-11-30 23:58:42 +0300

    neotree 3

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -96,7 +96,7 @@ require("lazy").setup({
 	-- Tree for files
 	{
 		"nvim-neo-tree/neo-tree.nvim",
-		branch = "v2.x",
+		branch = "v3.x",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			"nvim-tree/nvim-web-devicons",
