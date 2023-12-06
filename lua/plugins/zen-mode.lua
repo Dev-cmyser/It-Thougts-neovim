8c1566430097e1b4895964ac5aadd1323e4118c0
Dev-cmyser <kirrallt@mail.ru>
2023-12-06 15:20:23 +0300

    save zen

--- lua/plugins/zen-mode.lua
+++ lua/plugins/zen-mode.lua
@@ -20,24 +20,204 @@ require("true-zen").setup({
 			callbacks = {
 				open_pre = function()
 					ShowBufferline()
-					vim.wo.relativenumber = true -- включить относительные номера строк после открытия Ataraxis
+					vim.g.did_load_filetypes = 1
+					vim.g.formatoptions = "qrn1"
+					vim.opt.showmode = false
+					vim.opt.updatetime = 100
+					vim.wo.signcolumn = "yes"
+					vim.opt.scrolloff = 20
+
+					-- vim.opt.cmdheight = 0 -- ОТключить полосу внизу где символ и строка
+					vim.opt.wrap = true -- Включить перенос строк, игнорируя целостность слов
+					-- vim.opt.linebreak = true -- Включить перенос строк, сохраняя целостность слов
+					vim.opt.virtualedit = "block"
+					vim.opt.undofile = true
+					vim.opt.shell = "/bin/zsh"
+					vim.opt.swapfile = false
+
+					-- Вы можете также установить уровень сворачивания, например 'foldlevel'
+					vim.opt.foldmethod = "expr"
+					vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+					vim.wo.foldlevel = 99
+					-- Mouse
+					vim.opt.mouse = "a"
+					vim.opt.mousefocus = true
+					vim.opt.mousehide = true -- Спрятать курсор мыши когда набираем текст
+
+					-- Line Numbers
 					vim.opt.number = true
+					vim.opt.relativenumber = true
+
+					-- Splits
+					vim.opt.splitbelow = true
+					vim.opt.splitright = true
+					-- Clipboard
+					vim.opt.clipboard = "unnamedplus"
+					vim.api.nvim_set_keymap("x", "p", '"_dP', { noremap = true, silent = true })
+					-- Shorter messages
+					vim.opt.shortmess:append("c")
+
+					-- Indent Settings
+					vim.opt.expandtab = true
+					vim.opt.shiftwidth = 4
+					vim.opt.tabstop = 4
+					vim.opt.softtabstop = 4
+					vim.opt.smartindent = true
+					vim.opt.textwidth = 80
+
+					vim.cmd([[highlight clear LineNr]])
+					vim.cmd([[highlight clear SignColumn]])
 				end,
 
 				open_pos = function()
 					ShowBufferline()
-					vim.wo.relativenumber = true -- включить относительные номера строк после открытия Ataraxis
+					vim.g.did_load_filetypes = 1
+					vim.g.formatoptions = "qrn1"
+					vim.opt.showmode = false
+					vim.opt.updatetime = 100
+					vim.wo.signcolumn = "yes"
+					vim.opt.scrolloff = 20
+
+					-- vim.opt.cmdheight = 0 -- ОТключить полосу внизу где символ и строка
+					vim.opt.wrap = true -- Включить перенос строк, игнорируя целостность слов
+					-- vim.opt.linebreak = true -- Включить перенос строк, сохраняя целостность слов
+					vim.opt.virtualedit = "block"
+					vim.opt.undofile = true
+					vim.opt.shell = "/bin/zsh"
+					vim.opt.swapfile = false
+
+					-- Вы можете также установить уровень сворачивания, например 'foldlevel'
+					vim.opt.foldmethod = "expr"
+					vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+					vim.wo.foldlevel = 99
+					-- Mouse
+					vim.opt.mouse = "a"
+					vim.opt.mousefocus = true
+					vim.opt.mousehide = true -- Спрятать курсор мыши когда набираем текст
+
+					-- Line Numbers
 					vim.opt.number = true
+					vim.opt.relativenumber = true
+
+					-- Splits
+					vim.opt.splitbelow = true
+					vim.opt.splitright = true
+					-- Clipboard
+					vim.opt.clipboard = "unnamedplus"
+					vim.api.nvim_set_keymap("x", "p", '"_dP', { noremap = true, silent = true })
+					-- Shorter messages
+					vim.opt.shortmess:append("c")
+
+					-- Indent Settings
+					vim.opt.expandtab = true
+					vim.opt.shiftwidth = 4
+					vim.opt.tabstop = 4
+					vim.opt.softtabstop = 4
+					vim.opt.smartindent = true
+					vim.opt.textwidth = 80
+
+					vim.cmd([[highlight clear LineNr]])
+					vim.cmd([[highlight clear SignColumn]])
 				end,
 				close_pre = function()
 					ShowBufferline()
-					vim.wo.relativenumber = true -- включить относительные номера строк после открытия Ataraxis
+					vim.g.did_load_filetypes = 1
+					vim.g.formatoptions = "qrn1"
+					vim.opt.showmode = false
+					vim.opt.updatetime = 100
+					vim.wo.signcolumn = "yes"
+					vim.opt.scrolloff = 20
+
+					-- vim.opt.cmdheight = 0 -- ОТключить полосу внизу где символ и строка
+					vim.opt.wrap = true -- Включить перенос строк, игнорируя целостность слов
+					-- vim.opt.linebreak = true -- Включить перенос строк, сохраняя целостность слов
+					vim.opt.virtualedit = "block"
+					vim.opt.undofile = true
+					vim.opt.shell = "/bin/zsh"
+					vim.opt.swapfile = false
+
+					-- Вы можете также установить уровень сворачивания, например 'foldlevel'
+					vim.opt.foldmethod = "expr"
+					vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+					vim.wo.foldlevel = 99
+					-- Mouse
+					vim.opt.mouse = "a"
+					vim.opt.mousefocus = true
+					vim.opt.mousehide = true -- Спрятать курсор мыши когда набираем текст
+
+					-- Line Numbers
 					vim.opt.number = true
+					vim.opt.relativenumber = true
+
+					-- Splits
+					vim.opt.splitbelow = true
+					vim.opt.splitright = true
+					-- Clipboard
+					vim.opt.clipboard = "unnamedplus"
+					vim.api.nvim_set_keymap("x", "p", '"_dP', { noremap = true, silent = true })
+					-- Shorter messages
+					vim.opt.shortmess:append("c")
+
+					-- Indent Settings
+					vim.opt.expandtab = true
+					vim.opt.shiftwidth = 4
+					vim.opt.tabstop = 4
+					vim.opt.softtabstop = 4
+					vim.opt.smartindent = true
+					vim.opt.textwidth = 80
+
+					vim.cmd([[highlight clear LineNr]])
+					vim.cmd([[highlight clear SignColumn]])
 				end,
 
 				close_pos = function()
-					vim.wo.relativenumber = true -- выключить относительные номера строк после закрытия Ataraxis
+					vim.g.did_load_filetypes = 1
+					vim.g.formatoptions = "qrn1"
+					vim.opt.showmode = false
+					vim.opt.updatetime = 100
+					vim.wo.signcolumn = "yes"
+					vim.opt.scrolloff = 20
+
+					-- vim.opt.cmdheight = 0 -- ОТключить полосу внизу где символ и строка
+					vim.opt.wrap = true -- Включить перенос строк, игнорируя целостность слов
+					-- vim.opt.linebreak = true -- Включить перенос строк, сохраняя целостность слов
+					vim.opt.virtualedit = "block"
+					vim.opt.undofile = true
+					vim.opt.shell = "/bin/zsh"
+					vim.opt.swapfile = false
+
+					-- Вы можете также установить уровень сворачивания, например 'foldlevel'
+					vim.opt.foldmethod = "expr"
+					vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+					vim.wo.foldlevel = 99
+					-- Mouse
+					vim.opt.mouse = "a"
+					vim.opt.mousefocus = true
+					vim.opt.mousehide = true -- Спрятать курсор мыши когда набираем текст
+
+					-- Line Numbers
 					vim.opt.number = true
+					vim.opt.relativenumber = true
+
+					-- Splits
+					vim.opt.splitbelow = true
+					vim.opt.splitright = true
+					-- Clipboard
+					vim.opt.clipboard = "unnamedplus"
+					vim.api.nvim_set_keymap("x", "p", '"_dP', { noremap = true, silent = true })
+					-- Shorter messages
+					vim.opt.shortmess:append("c")
+
+					-- Indent Settings
+					vim.opt.expandtab = true
+					vim.opt.shiftwidth = 4
+					vim.opt.tabstop = 4
+					vim.opt.softtabstop = 4
+					vim.opt.smartindent = true
+					vim.opt.textwidth = 80
+
+					vim.cmd([[highlight clear LineNr]])
+					vim.cmd([[highlight clear SignColumn]])
 				end,
 			},
 		},
