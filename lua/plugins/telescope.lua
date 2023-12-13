590d1ce200c5edc1c91992663655bece9229ffb1
Dev-cmyser <kirrallt@mail.ru>
2023-12-13 16:52:34 +0300

    grep и поиск по всем файлам

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,19 @@
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fF", function()
+	builtin.find_files({
+		hidden = true,
+		no_ignore = true,
+	})
+end, {})
 vim.keymap.set("n", "<leader>fw", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>fW", function()
+	builtin.live_grep({
+		additional_args = function(opts)
+			return { "--hidden", "-u" }
+		end,
+	})
+end, {})
 vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 vim.keymap.set("n", "<leader>gb", builtin.git_branches, {})
