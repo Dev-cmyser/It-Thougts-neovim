2cc28e9e0bf73d7ac3988718c55bec225263f286
Dev-cmyser <kirrallt@mail.ru>
2024-01-23 15:34:21 +0300

    сонар

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -12,6 +12,7 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
+	{ "https://gitlab.com/schrieveslaach/sonarlint.nvim", lazy = false },
 	{
 		"folke/todo-comments.nvim",
 		dependencies = { "nvim-lua/plenary.nvim" },
---
+++ lua/plugins/sonarlint.lua
@@ -0,0 +1,17 @@
+require("sonarlint").setup({
+	server = {
+		cmd = {
+			"sonarlint-language-server",
+			-- Ensure that sonarlint-language-server uses stdio channel
+			"-stdio",
+			"-analyzers",
+			-- paths to the analyzers you need, using those for python and java in this example
+			vim.fn.expand("$MASON/share/sonarlint-analyzers/sonartypescript.jar"),
+			vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarcfamily.jar"),
+		},
+	},
+	filetypes = {
+		-- Tested and working
+		"typescript",
+	},
+})
